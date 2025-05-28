9b1ed9199d563637e7bf318dc97f598de78e96e1
DiamondGotCat <diamondgotcat@DGC-MacBook-Local.local>
2025-05-29 07:34:57 +0900

    no description

--- app.py
+++ app.py
@@ -389,7 +389,7 @@ def index_page():
 
 @app.route('/error/<path:code>/')
 def error_page(code):
-    return render_template('error.html', enumber=code, ename=f"Error Page")
+    return render_template('error.html', enumber=code, ename=f"Error Page"), int(code)
 
 @app.route('/api/v1/')
 def api_index():
